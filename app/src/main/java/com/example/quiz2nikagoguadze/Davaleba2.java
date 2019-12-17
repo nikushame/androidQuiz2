0528456608c37b48739d1329f6a6e6b35a4368a6
unknown <nmestvirishvili22@gmail.com>
2019-12-18 01:19:25 +0400

    initial commit

---
+++ app/src/main/java/com/example/quiz2nikagoguadze/Davaleba2.java
@@ -0,0 +1,14 @@
+package com.example.quiz2nikagoguadze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Davaleba2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_davaleba2);
+    }
+}
